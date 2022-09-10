5e5cf4464b0d34ba9fd7947fc8bfe7221c1a00d1
Marcos Vinícius da Silva <marcosvinicius.correiadasilva@providers.glovoapp.com>
2022-09-10 18:24:36 +0200

    allow test files to be TS

--- tests/e2e/specs/0-smoke-test.ts
+++ tests/e2e/specs/0-smoke-test.ts
@@ -1,4 +1,4 @@
-describe("Giving credit to author", () => {
+describe("Smoke Test", () => {
   it("Performs a smoke test on the app", () => {
     cy.visit("/");
     cy.contains("p", "Written by Evan You");
